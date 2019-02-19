a7ea255351b44642c5f6b3cb6eabfa93fce99350
郑江荣 <362675035@qq.com>
2019-02-19 21:50:14 +0800

    update

--- src/main/java/com/farwolf/jpush/module/JpushModule.java
+++ src/main/java/com/farwolf/jpush/module/JpushModule.java
@@ -2,8 +2,11 @@ package com.farwolf.jpush.module;
 
 
 import com.farwolf.weex.annotation.WeexModule;
+import com.farwolf.weex.base.WXModuleBase;
 import com.taobao.weex.annotation.JSMethod;
-import com.taobao.weex.common.WXModule;
+import com.taobao.weex.bridge.JSCallback;
+
+import java.util.HashMap;
 
 import cn.jpush.android.api.JPushInterface;
 
@@ -13,10 +16,19 @@ import cn.jpush.android.api.JPushInterface;
  * @create_date 2018/4/20
  */
 @WeexModule(name = "jpush")
-public class JpushModule extends WXModule {
+public class JpushModule extends WXModuleBase {
 
     @JSMethod(uiThread =false)
     public String getJPushId(){
         return  JPushInterface.getRegistrationID(mWXSDKInstance.getContext());
     }
+
+    @JSMethod
+    public void regist(HashMap m, JSCallback callback){
+
+
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(getContext().getApplicationContext());     		// 初始化 JPush
+    }
+
 }
